8e9ca456718ce1084f42a956a2d51dfd78d44fc6
RenatoAAraujo <renatoassuncaodearaujo@gmail.com>
2020-05-29 20:49:32 -0300

    log improvement

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
 def subsets_with_sum(numbers_set:list, target:int, with_replacement:bool):
-    print(f"The numbers that will be used to reach the sum of {target} are:\n{numbers_set}\nThe sets can have replaced numbers? {with_replacement}.")
-    print("The larger of \"target\" is the longer this will take")
+    print(f"The numbers that will be used to reach the sum of {target} are:\n{numbers_set}\nThe sets can have replaced numbers? {with_replacement}.\n")
+    print(f"The larger the \"target\" ({target}) is the longer this will take.")
 
     def generator(index, generation_list, generated_set, generation_target):
         if generation_target == sum(generation_list):
