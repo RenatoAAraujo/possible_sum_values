63ae5f9feae299580440679ccad7ebfee2720227
Renato <renatoassuncaodearaujo@gmail.com>
2020-05-31 02:24:57 -0300

    improved log

--- export_data.py
+++ export_data.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 
 def get_combination_lists(target, numeric_range):
-    print(f"{total_numbers_to_be_used} numbers will be used to try to reach the sum of {target}.\n")
+    print(f"{total_numbers_to_be_used} numbers will be used to try to reach the sum of {target}.")
     print(f"The larger the \"target\" ({target}) is the longer this will take.")
 
     combinations_list = list(sub_lists_with_sum(total_numbers_to_be_used=total_numbers_to_be_used, target=target))
     combinations_list = filter_numbers_used(combinations_list=combinations_list, numeric_range=numeric_range)
-    print(f"total permutations for target ({target}): {len(combinations_list)}")
+    print(f"total permutations for target ({target}): {len(combinations_list)}\n")
 
     df_list = pd.DataFrame({"possible_set": combinations_list})
     df_list["total"] = [sum(x) for x in df_list["possible_set"]]
