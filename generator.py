3faac78b49b31b9d592161b7eccb7bffb1c6356d
RenatoAAraujo <renatoassuncaodearaujo@gmail.com>
2020-06-01 20:47:24 -0300

    improved efficiency

--- generator.py
+++ generator.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def sub_lists_with_sum(total_numbers_to_be_used: bool, target: int):
     if total_numbers_to_be_used <= 0:
         raise ValueError("\"total_numbers\" must be bigger than 0.")
@@ -10,11 +13,17 @@ def sub_lists_with_sum(total_numbers_to_be_used: bool, target: int):
 
 
 def filter_numbers_used(combinations_list, numeric_range):
+    list_len = len(combinations_list)
     filtered_list = []
 
     index = 0
-    for i in range(len(combinations_list)):
-        if not set(combinations_list[index]).issubset(numeric_range) or sorted(combinations_list[index]) in filtered_list:
+    for i in range(list_len):
+        if i % 10000 == 0:
+            print(f"[{datetime.now()}] {i}/{list_len}")
+
+        if not set(combinations_list[index]).issubset(numeric_range):
+            combinations_list.pop(index)
+        elif sorted(combinations_list[index]) in filtered_list:
             combinations_list.pop(index)
         else:
             filtered_list.append(sorted(combinations_list[index]))
