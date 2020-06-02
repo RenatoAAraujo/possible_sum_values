3fa40b8492d6be692f7fd5268b7ba0b62d63ac7f
RenatoAAraujo <renatoassuncaodearaujo@gmail.com>
2020-06-01 22:10:57 -0300

    save files

---
+++ temporary_files.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import pickle
+
+
+def save_file(ls: list, name: str):
+    print(f"[{datetime.now()}] Saving {name} pickle file")
+    with open(f"{name}.pkl", "wb") as f:
+        pickle.dump(ls, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_file(name: str):
+    print(f"[{datetime.now()}] Loading {name} pickle file")
+    with open(f"{name}.pkl", "rb") as f:
+        return pickle.load(f)
