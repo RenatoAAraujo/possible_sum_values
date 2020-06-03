72ae534c7b157fa4dd7d724054a1fa7b19340425
RenatoAAraujo <renatoassuncaodearaujo@gmail.com>
2020-06-03 10:47:58 -0300

    log improvement (thread pool)

--- generator.py
+++ generator.py
@@ -16,12 +16,12 @@ def sub_lists_with_sum(total_numbers_to_be_used: bool, target: int):
 
 
 def filter_numbers_used(combinations_list, target, numeric_range, save_filtered_lists):
-    print(f"[{datetime.now()}] Filtering list values out of range. List contains {len(combinations_list)} values")
+    print(f"[{datetime.now()}] [{target}] Filtering list values out of range. List contains {len(combinations_list)} values")
     combinations_list = np.array(combinations_list)
     combinations_list = combinations_list[(combinations_list <= max(numeric_range)).all(axis=1)].tolist()
     if save_filtered_lists:
         save_file(combinations_list, f"target_{target}")
-    print(f"[{datetime.now()}] After filtering list contains {len(combinations_list)} values")
+    print(f"[{datetime.now()}] [{target}] After filtering list contains {len(combinations_list)} values")
 
     list_len = len(combinations_list)
     filtered_list = []
@@ -29,10 +29,12 @@ def filter_numbers_used(combinations_list, target, numeric_range, save_filtered_
     index = 0
     for i in range(list_len):
         if i % 10000 == 0:
-            print(f"[{datetime.now()}] {i}/{list_len}")
             if save_filtered_lists:
                 save_file(filtered_list, f"temp_filtered_list_{target}")
                 save_file(index, f"temp_index_{target}")
+                print(f"[{datetime.now()}] [{target}] temp filtes created  {i}/{list_len}")
+            else:
+                print(f"[{datetime.now()}] {i}/{list_len}")
 
         if sorted(combinations_list[index]) in filtered_list:
             combinations_list.pop(index)
