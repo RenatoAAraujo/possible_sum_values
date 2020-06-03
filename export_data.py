0f313217607a8708fa345dbea4925b3d747d8c93
RenatoAAraujo <renatoassuncaodearaujo@gmail.com>
2020-06-03 10:14:05 -0300

    threadpools

--- export_data.py
+++ export_data.py
@@ -1,6 +1,8 @@
 from generator import sub_lists_with_sum, filter_numbers_used
 
+from concurrent.futures import ThreadPoolExecutor, as_completed, wait
 from datetime import datetime
+from multiprocessing import cpu_count
 import pandas as pd
 
 
@@ -15,7 +17,7 @@ def get_combination_lists(target, numeric_range, save_filtered_lists):
     df_list = pd.DataFrame({"possible_set": combinations_list})
     df_list["total"] = [sum(x) for x in df_list["possible_set"]]
 
-    df_list.to_csv(f"[{datetime.now()}] possible_combinations_{target}.csv", sep=",", header=True, index=False)
+    df_list.to_csv(f"possible_combinations_{target}.csv", sep=",", header=True, index=False)
 
     return df_list
 
@@ -23,12 +25,20 @@ def get_combination_lists(target, numeric_range, save_filtered_lists):
 if __name__ == "__main__":
     make_temporary_files = False
     total_numbers_to_be_used = 6
-    numbers_to_be_used = range(0, 40)
+    numbers_to_be_used = range(0, 5)
 
+    if cpu_count() > 3:
+        executor = ThreadPoolExecutor(cpu_count() - 3)
+    else:
+        executor = ThreadPoolExecutor(cpu_count() - 1)
+    complete_df_list = list()
     complete_df = pd.DataFrame()
-    for t in range(60, 71)[::-1]:
-        df = get_combination_lists(t, numbers_to_be_used, make_temporary_files)
-        complete_df = pd.concat([complete_df, df]).sort_values(by=["total"], ascending=False)
+    
+    for t in range(0, 10)[::-1]:
+        complete_df_list.append(executor.submit(get_combination_lists, t, numbers_to_be_used, make_temporary_files))
+    for i in as_completed(complete_df_list):
+        complete_df = pd.concat([complete_df, i.result()]).sort_values(by=["total"], ascending=False)
+    
     complete_df.to_csv(f"possible_combinations.csv", sep=",", header=True, index=False)
 
     print("Done!")
