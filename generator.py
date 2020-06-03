e20c8863ce97711aa02dc65f493b04a7043699ea
RenatoAAraujo <renatoassuncaodearaujo@gmail.com>
2020-06-03 17:29:09 -0300

    optimazations using numpy

--- generator.py
+++ generator.py
@@ -18,28 +18,12 @@ def sub_lists_with_sum(total_numbers_to_be_used: bool, target: int):
 def filter_numbers_used(combinations_list, target, numeric_range, save_filtered_lists):
     print(f"[{datetime.now()}] [{target}] Filtering list values out of range. List contains {len(combinations_list)} values")
     combinations_list = np.array(combinations_list)
-    combinations_list = combinations_list[(combinations_list <= max(numeric_range)).all(axis=1)].tolist()
+    combinations_list = combinations_list[(combinations_list <= max(numeric_range)).all(axis=1)]
+    combinations_list.sort(axis=1)
     if save_filtered_lists:
         save_file(combinations_list, f"target_{target}")
     print(f"[{datetime.now()}] [{target}] After filtering list contains {len(combinations_list)} values")
 
-    list_len = len(combinations_list)
-    filtered_list = []
+    filtered_list = np.unique(combinations_list, axis=0)
 
-    index = 0
-    for i in range(list_len):
-        if i % 10000 == 0:
-            if save_filtered_lists:
-                save_file(filtered_list, f"temp_filtered_list_{target}")
-                save_file(index, f"temp_index_{target}")
-                print(f"[{datetime.now()}] [{target}] temp filtes created  {i}/{list_len}")
-            else:
-                print(f"[{datetime.now()}] {i}/{list_len}")
-
-        if sorted(combinations_list[index]) in filtered_list:
-            combinations_list.pop(index)
-        else:
-            filtered_list.append(sorted(combinations_list[index]))
-            index += 1
-
-    return filtered_list
+    return filtered_list.tolist()
