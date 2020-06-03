1abd943ee264c0ad8ea138e922e2bb7f2205b912
RenatoAAraujo <renatoassuncaodearaujo@gmail.com>
2020-06-03 10:25:03 -0300

    temp files location

--- temporary_files.py
+++ temporary_files.py
@@ -4,11 +4,11 @@ import pickle
 
 def save_file(ls: list, name: str):
     print(f"[{datetime.now()}] Saving {name} pickle file")
-    with open(f"{name}.pkl", "wb") as f:
+    with open(f"temp/{name}.pkl", "wb") as f:
         pickle.dump(ls, f, pickle.HIGHEST_PROTOCOL)
 
 
 def load_file(name: str):
     print(f"[{datetime.now()}] Loading {name} pickle file")
-    with open(f"{name}.pkl", "rb") as f:
+    with open(f"temp/{name}.pkl", "rb") as f:
         return pickle.load(f)
