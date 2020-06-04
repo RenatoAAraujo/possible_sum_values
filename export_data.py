8febd641ed092fdee05a39d5c9d46a165930d8f3
RenatoAAraujo <renatoassuncaodearaujo@gmail.com>
2020-06-04 09:11:30 -0300

    variable renaming

--- export_data.py
+++ export_data.py
@@ -6,18 +6,19 @@ from multiprocessing import cpu_count
 import pandas as pd
 
 
-def get_combination_lists(target, numeric_range, replace, save_filtered_lists):
+def get_combination_lists(target, numeric_range, replace, save_files):
     print(f"[{datetime.now()}] {total_numbers_to_be_used} numbers will be used to try to reach the sum of {target}.")
     print(f"[{datetime.now()}] The larger the \"target\" ({target}) is the longer this will take.")
 
     combinations_list = list(sub_lists_with_sum(total_numbers_to_be_used, target))
-    combinations_list = filter_numbers_used(combinations_list, target, numeric_range, replace, save_filtered_lists)
+    combinations_list = filter_numbers_used(combinations_list, target, numeric_range, replace, save_files)
     print(f"[{datetime.now()}] total combinations for target ({target}): {len(combinations_list)}")
 
     df_list = pd.DataFrame({"possible_set": combinations_list})
     df_list["total"] = [sum(x) for x in df_list["possible_set"]]
 
-    df_list.to_csv(f"possible_combinations_{target}.csv", sep=",", header=True, index=False)
+    if save_files:
+        df_list.to_csv(f"possible_combinations_{target}.csv", sep=",", header=True, index=False)
 
     return df_list
 
--- generator.py
+++ generator.py
@@ -15,13 +15,13 @@ def sub_lists_with_sum(total_numbers_to_be_used: bool, target: int):
                 yield [value, ] + permutation
 
 
-def filter_numbers_used(combinations_list, target, numeric_range, replace, save_filtered_lists):
+def filter_numbers_used(combinations_list, target, numeric_range, replace, save_files):
     print(f"[{datetime.now()}] [{target}] Filtering list values out of range. List contains {len(combinations_list)} values")
     combinations_list = np.array(combinations_list)
     combinations_list = combinations_list[(combinations_list > 0).all(axis=1)]
     combinations_list = combinations_list[(combinations_list <= max(numeric_range)).all(axis=1)]
     combinations_list.sort(axis=1)
-    if save_filtered_lists:
+    if save_files:
         save_file(combinations_list, f"target_{target}")
     print(f"[{datetime.now()}] [{target}] After filtering list contains {len(combinations_list)} values")
 
